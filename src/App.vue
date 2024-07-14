dcfdc63e44156143e1ce7860d0c308d735e36f49
velimir <velimir.mueller@googlemail.com>
2024-07-14 20:46:05 +0200

    use default layout with tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
-    <h1>test</h1>
-  </div>
+  <DefaultLayout>
+    <h1 class="text-4xl font-bold bg-red-200 h-full">Hello World!</h1>
+  </DefaultLayout>
 </template>
+
+<script setup lang="ts">
+import DefaultLayout from '@/layout/DefaultLayout.vue'
+</script>
---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col h-screen max-h-screen">
+    <div class="h-12 bg-blue-400">NAVBAR</div>
+    <div class="h-full overflow-y-scroll">
+      <slot />
+    </div>
+    <div class="h-12 p-4 bg-blue-400">FOOTER</div>
+  </div>
+</template>
