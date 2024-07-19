21d158f3d3b95b8f2331153abff4abc78858e5eb
velimir <velimir.mueller@googlemail.com>
2024-07-19 22:38:22 +0200

    use keyframes globally

---
+++ src/components/AccessbleButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button 
+    :class="`${props.isAnimated ? 'sign-in' : ''} w-48 rounded-md text-cyan-900 h-1/2 mr-4 border-2 border-cyan-600 bg-white`">
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  isAnimated: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+
+  text: {
+    type: String,
+    required: true
+  },
+
+  type: {
+    type: String,
+    required: false,
+    default: 'button',
+    validator(value: string) {
+      // The value must match one of these strings
+      return ['button', 'submit', 'reset'].includes(value)
+    }
+  }
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+
 import './assets/tailwind.css'
+import './assets/keyframes.scss'
 
 const app = createApp(App)
 
