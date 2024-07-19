c137d181674c075d8f12dc5db12706758cda53d2
velimir <velimir.mueller@googlemail.com>
2024-07-20 00:44:07 +0200

    add mobile support

--- src/layout/components/DefaultIcon.vue
+++ src/layout/components/DefaultIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <a 
-    class="logo block flex items-center p-4 px-8 text-xl h-2/3 mt-auto mb-auto items-center ml-2 rounded-lg p-2 justify-start bg-white border-cyan-700 border-2 text-cyan-900"
+    class="logo block flex items-center p-4 px-4 text-xl h-2/3 mt-auto mb-auto items-center rounded-lg p-2 justify-start bg-white border-cyan-700 border-2 text-cyan-900"
     href="#">
     <AccessibleIcon
       icon="book" 
---
+++ src/layout/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav 
+    role="navigation"
+    class="flex flex-row w-full items-center bg-blue-400 h-24 px-4">
+    <template class="hidden lg:flex w-full h-full">
+      <DefaultIcon 
+        v-if="props.hasDefaultIcon"
+        class="justfy-center items-center" />
+      <DefaultLinkList />
+    </template>
+    <AccessibleButton 
+      class="hover:text-cyan-900 ml-auto lg:w-48 w-36 lg:mr-0" 
+      :is-animated="true" 
+      text="SIGN IN" 
+      type="button" />
+  </nav>
+</template>
+
+<script setup lang="ts">
+import AccessibleButton from '@/components/AccessbleButton.vue'
+import DefaultIcon from '@/layout/components/DefaultIcon.vue'
+import DefaultLinkList from '@/layout/components/DefaultLinkList.vue'
+
+const props = defineProps({
+  hasDefaultIcon: {
+    type: Boolean,
+    required: false,
+    default: false
+  }
+})
+
+</script>
