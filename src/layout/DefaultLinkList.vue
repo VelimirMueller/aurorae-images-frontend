86775527be9a15857dd15b4fdcc5a8a312595894
velimir <velimir.mueller@googlemail.com>
2024-07-19 23:53:30 +0200

    refactor nav links into own sfc

---
+++ src/layout/DefaultLinkList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="w-full flex h-e items-center justify-center hover:text-bold">
+    <template 
+      v-for="link in props.navLinks"
+      :key="link.text">
+      <li class="mr-12 hover:text-cyan-900 hover:underline hover:-translate-y-px hover:translate-x-px transition-all duration-900 cubic-beziere delay-200">
+        <a class="link mx-4 h-24" :href="link.href">
+          {{ link.text ?? '' }}
+        </a>
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  navLinks: {
+    type: Object,
+    required: true
+  }
+})
+</script>
--- src/layout/NavBar.vue
+++ src/layout/NavBar.vue
@@ -3,17 +3,7 @@
     role="navigation"
     class="flex w-full items-center bg-blue-400 h-24">
     <DefaultIcon v-if="props.hasDefaultIcon" />
-    <ul class="w-full flex h-e items-center justify-center hover:text-bold">
-      <template 
-        v-for="link in props.navLinks"
-        :key="link.text">
-        <li class="mr-12 hover:text-cyan-900 hover:underline hover:-translate-y-px hover:translate-x-px transition-all duration-900 cubic-beziere delay-200">
-          <a class="link mx-4 h-24" :href="link.href">
-            {{ link.text ?? '' }}
-          </a>
-        </li>
-      </template>
-    </ul>
+    <DefaultLinkList :nav-links="props.navLinks" />
     <AccessibleButton class="hover:text-cyan-900" :is-animated="true" text="SIGN IN" type="button" />
   </nav>
 </template>
@@ -21,6 +11,7 @@
 <script setup lang="ts">
 import AccessibleButton from '@/components/AccessbleButton.vue'
 import DefaultIcon from '@/layout/DefaultIcon.vue'
+import DefaultLinkList from '@/layout/DefaultLinkList.vue'
 
 const props = defineProps({
   navLinks: {
