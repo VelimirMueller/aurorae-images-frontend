6ff396465a116570f654c5218ef472060b5d4c17
velimir <velimir.mueller@googlemail.com>
2024-08-16 16:59:34 +0200

    add image uploader component

---
+++ src/components/ImageUploader.vue
@@ -0,0 +1,48 @@
+<template>
+  <form class="h-full w-full flex flex-col justify-center items-center">
+    <template v-if="previewUrl">
+      <div class="bg-white w-full h-full relative">
+        <div 
+          class="absolute right-0 text-4xl mr-2 mt-2 cursor-pointer" 
+          @click="resetImage()">
+          x
+        </div>
+        <img 
+          class="w-full h-full" 
+          :src="previewUrl" 
+          alt="uploaded image preview" />
+      </div>
+    </template>
+    <input 
+      ref="inputFile" 
+      class="w-full bg-white" 
+      type="file" 
+      @change="setPreviewUrl()" />
+  </form>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+ 
+interface InputFile {
+  files: Array<Blob|MediaSource>
+}
+
+const inputFile: Ref<InputFile | undefined> = ref(undefined)
+const previewUrl: Ref<string> = ref('')
+
+const setPreviewUrl = (): void => {
+  try {
+    if (inputFile.value) {
+      previewUrl.value = window.URL.createObjectURL(inputFile.value.files[0])
+    }
+  } catch (err) {
+    console.error('Could not set file due to: ', err)
+  }
+}
+
+const resetImage = () => {
+  previewUrl.value = ''
+}
+</script>
