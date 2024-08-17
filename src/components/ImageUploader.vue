5b89184a9e808e64ef34f5b7e76a6eb76cda7969
velimir <velimir.mueller@googlemail.com>
2024-08-17 11:15:25 +0200

    tweak image uploader component

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -2,30 +2,40 @@
   <form class="h-full w-full flex flex-col justify-center items-center">
     <div class="bg-white w-full h-full relative">
       <template v-if="!previewUrl">
-        <div class="w-full h-full flex flex-col justify-center items-center border">
-          <AccessibleIcon class="text-blue-400" icon="upload" :size="[248, 248]" />
-          <h2 class="text-4xl">
-            upload image
-          </h2>
+        <div class="w-full h-full flex flex-col justify-center items-center">
+          <label 
+            for="image-upload" 
+            class="custom-file-upload">
+            <AccessibleIcon 
+              class="text-blue-400" 
+              icon="upload" 
+              :size="[248, 248]" />
+            <h2 class="text-4xl">
+              upload image
+            </h2>
+            <input 
+              id="image-upload"
+              ref="inputFile" 
+              class="w-full bg-white" 
+              type="file" 
+              @change="setPreviewUrl()" />
+          </label>
         </div>
       </template>
       <template v-else>
         <div 
-          class="absolute right-0 text-4xl mr-2 mt-2 cursor-pointer" 
+          class="absolute right-0 text-4xl cursor-pointer mr-4 mt-4 rounded-full bg-gray-900 text-white border-2 border-white hover:bg-gray-700 w-12 h-12 flex items-center justify-center" 
           @click="resetImage()">
           x
         </div>
-        <img 
-          class="w-full h-full" 
-          :src="previewUrl" 
-          alt="uploaded image preview" />
+        <div class="w-full h-full flex items-center justify-center p-8">
+          <img  
+            class="w-full h-full"
+            :src="previewUrl" 
+            alt="uploaded image preview" />
+        </div>
       </template>
     </div>
-    <input 
-      ref="inputFile" 
-      class="w-full bg-white" 
-      type="file" 
-      @change="setPreviewUrl()" />
   </form>
 </template>
 
@@ -55,3 +65,14 @@ const resetImage = () => {
   previewUrl.value = ''
 }
 </script>
+
+<style scoped lang="scss">
+input[type="file"] {
+  display: none;
+}
+
+.custom-file-upload {
+  display: inline-block;
+  cursor: pointer;
+}
+</style>
