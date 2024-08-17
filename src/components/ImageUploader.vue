3f773f257ce6f0f8b6c53fd4606ec8ffbebbd4a6
velimir <velimir.mueller@googlemail.com>
2024-08-17 20:36:44 +0200

    debug image upload

--- src/components/ImageUploader.vue
+++ src/components/ImageUploader.vue
@@ -1,5 +1,8 @@
 <template>
-  <form class="h-full w-full flex flex-col justify-center items-center">
+  <form 
+    ref="uploadForm"
+    class="h-full w-full flex flex-col justify-center items-center"
+    @submit.prevent>
     <div class="bg-white w-full h-full relative">
       <template v-if="!previewUrl">
         <div class="w-full h-full flex flex-col justify-center items-center">
@@ -13,9 +16,10 @@
             <h2 class="text-4xl">
               upload image
             </h2>
-            <input 
+            <input
               id="image-upload"
               ref="inputFile" 
+              name="image"
               class="w-full bg-white" 
               type="file" 
               @change="setPreviewUrl()" />
@@ -50,11 +54,22 @@ interface InputFile {
 
 const inputFile: Ref<InputFile | undefined> = ref(undefined)
 const previewUrl: Ref<string> = ref('')
+const uploadForm: Ref<HTMLFormElement | undefined> = ref()
 
-const setPreviewUrl = (): void => {
+const setPreviewUrl = async (): Promise<void> => {
   try {
-    if (inputFile.value) {
+    if (inputFile.value && uploadForm.value) {
       previewUrl.value = window.URL.createObjectURL(inputFile.value.files[0])
+      const payload = new FormData(uploadForm.value)
+      
+      fetch('http://localhost:8000/upload/', {
+        method: 'POST',
+        body: payload
+      })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+        })      
     }
   } catch (err) {
     console.error('Could not set file due to: ', err)
