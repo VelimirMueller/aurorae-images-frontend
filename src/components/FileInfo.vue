97e91204d57c9a0e4c7b9778d278d51532c86696
velimir <velimir.mueller@googlemail.com>
2024-10-15 20:59:03 +0200

    Refactor File Info into own component

---
+++ src/components/FileInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h-2/5 w-full text-white">
+    <ul 
+      v-if="props.file"
+      class="h-full">
+      <li>
+        {{ 'FILENAME: ' + file.filename }}
+      </li>
+      <li>
+        {{ 'FILESIZE: ' + (file.size / 1024 / 1024).toFixed(2) + 'MB' }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { type PropType } from 'vue'
+
+interface FileInfo {
+  filename: string
+  size: number
+}
+
+const props = defineProps({
+  file: {
+    type: Object as PropType<FileInfo>,
+    required: true
+  }
+})
+</script>
