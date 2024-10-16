5ecca77e1033e286dcb3d89876d6d03f82c4cbf8
velimir <velimir.mueller@googlemail.com>
2024-10-16 21:14:17 +0200

    update imports after file renaming

--- src/components/ImageSearch.vue
+++ src/components/ImageSearch.vue
@@ -10,7 +10,7 @@
     <input 
       id="query"
       v-model="searchTerm"
-      class="w-4/5 h-full rounded-bl-lg p-2 text-lg focus:outline-none focus:border-sky-500 focus:ring-2 focus:ring-sky-500
+      class="w-4/5 h-full rounded-bl-md rounded-tl-md p-2 text-lg focus:outline-none focus:border-t focus:border-b focus:border-l focus:border-sky-500 focus:ring-2 focus:ring-sky-500
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
       invalid:border-pink-500 invalid:text-pink-600
       focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
---
+++ src/types/search.ts
@@ -0,0 +1,10 @@
+export interface SearchResult {
+  data?: {
+    topic_id: string
+    topic_status: string
+    topic_headline: string
+    topic_content: string
+    topic_link: string
+    topic_images: []
+  }
+}
