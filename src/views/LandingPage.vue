4fd31f039815b7f285e226bfaf8a2a66e596b96d
velimir <velimir.mueller@googlemail.com>
2024-10-16 22:07:27 +0200

    use new structure

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,25 +1,21 @@
- f<template>
+<template>
   <LpHero>
     <template #hero-content>
-      <div class="flex h-full">
-        <div class="w-full h-full">
-          <section 
-            id="section:ImageUploader"
-            class="h-full">
-            <div class="h-full w-full p-12 bg-gray-200 grid grid-cols-2 rounded-md">
-              <ImageUploader 
-                @reset="() => {}"
-                @set-file="val => {}" />
-              <div class="flex flex-col justify-center items-center text-white h-full w-full bg-gray-800 rounded-md">
-                <div class="h-20 w-full px-4 py-2 mt-2">
-                  <ImageSearch @submit="scrapeData" />
-                </div>
-                <ImageList :search-result="searchResult" />
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
+      <ImageUploaderWrapper>
+        <template #upload>
+          <ImageUploader 
+            @reset="() => {}"
+            @set-file="val => {}" />
+        </template>
+
+        <template #search>
+          <ImageSearch @submit="scrapeData" />
+        </template>
+        
+        <template #list>
+          <ImageList :search-result="searchResult" />
+        </template>
+      </ImageUploaderWrapper>
     </template>
   </LpHero>
 </template>
@@ -29,8 +25,9 @@ import LpHero from '@/components/landingPage/LpHero.vue'
 import ImageList from '@/components/ImageList.vue'
 import ImageSearch from '@/components/ImageSearch.vue'
 import ImageUploader from '@/components/ImageUploader.vue'
-import { ref } from 'vue'
-import type { Ref } from 'vue'
+import ImageUploaderWrapper from '@/components/ImageUploaderWrapper.vue'
+import { ref, type Ref } from 'vue'
+import type { SearchResult } from '@/types'
 
 const searchResult: Ref<SearchResult|undefined> = ref()
 const scrapeData = (searchQuery: string) => {
