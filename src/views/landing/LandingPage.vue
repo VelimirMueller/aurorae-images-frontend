bba930e242c79a284549f8095138dde0f11cef17
velimir <velimir.mueller@googlemail.com>
2024-11-24 20:50:10 +0100

    add better error message

--- src/views/landing/LandingPage.vue
+++ src/views/landing/LandingPage.vue
@@ -37,7 +37,7 @@ const scrapeData = (searchQuery: string) => {
         searchResult.value = data
       })
   } catch (err) {
-    console.error(err)
+    console.error(`Something went wrong when scrapping data. Error: ${err}`)
   }
 }
 </script>
